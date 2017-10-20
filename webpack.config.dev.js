397e3352075a6c8ea235ff06c24f9ddcfe3f70f9
Nathaniel Riehl <nate@nateriehl.com>
2017-10-20 13:25:22 -0500

    Completed Module 8 of the PluralSight course.

---
+++ webpack.config.dev.js
@@ -0,0 +1,24 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+    debug: true,
+    devtool: 'inline-source-map',
+    noInfo: false,
+    entry: [
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [],
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+            {test: /\.css$/, loaders: ['style-loader','css-loader']}
+        ]
+    }
+}
