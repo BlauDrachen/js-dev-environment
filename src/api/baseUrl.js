b36c0b7ad530a22f448ac9b0d3a34240a23a7de7
Nathaniel Riehl <nate@nateriehl.com>
2017-10-30 18:49:01 -0500

    Setup minification and dist build script for production

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,15 @@
+// tslint:disable:curly max-line-length one-variable-per-declaration
+
 export default function getBaseUrl() {
-    const inDev = window.location.hostname === 'localhost';
-    return inDev ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
+}
+
+function getQueryStringParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,28 @@
 import path from 'path';
+import webpack from 'webpack';
 
 // tslint:disable:object-literal-sort-keys
 
 export default {
     debug: true,
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     noInfo: false,
     entry: [
         path.resolve(__dirname, 'src/index')
     ],
     target: 'web',
     output: {
-        path: path.resolve(__dirname, 'src'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
         filename: 'bundle.js'
     },
-    plugins: [],
+    plugins: [
+        // Eliminate Duplicate packages when generating bundle
+        new webpack.optimize.DedupePlugin(),
+
+        // Minify JS
+        new webpack.optimize.UglifyJsPlugin()
+    ],
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
